d889e51b5b73639cc8c03f748bbcd5391b94f2b1
Jeremia Kimelman <jeremia@calmatters.org>
2023-01-21 10:11:11 -0800

    Add layer toggles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,131 @@
 <script>
+	import { writable } from 'svelte/store';
 	import MapboxMap from '$lib/components/MapboxMap.svelte';
 
 	export let data = {};
-	let map = null
+	let map = null;
 	const { bbox, bike, d4, onstreet, signals } = data;
 
-	const layers = [
+	const layerToggles = writable([
 		{
-			label: 'On street parking',
-			toggle: isShown => {
-				if (isShown) {
-					map.setPaintProperty('onstreet', 'circle-opacity', 1)
-				} else {
-					map.setPaintProperty('onstreet', 'circle-opacity', 0)
-				}
-			}
+			id: 'd4',
+			label: 'District 4',
+			shown: true
+		},
+		{
+			id: 'street-parking',
+			label: 'Street parking',
+			shown: false
+		},
+		{
+			id: 'bike-routes',
+			label: 'Bike routes within D4',
+			shown: false
+		},
+		{
+			id: 'bike-routes-speed',
+			label: 'Road speeds along bike routes',
+			shown: false
 		}
-	]
+	]);
+
+	layerToggles.subscribe((next) => {
+		if (!map) return;
+		const d4 = next.find((d) => d.id === 'd4');
+		const street = next.find((d) => d.id === 'street-parking');
+		const bike = next.find((d) => d.id === 'bike-routes');
+		const bikeRoadSpeed = next.find((d) => d.id === 'bike-routes-speed');
+
+		if (d4.shown) {
+			map.setPaintProperty('d4-fill', 'fill-opacity', 0.1);
+			map.setPaintProperty('d4-stroke', 'line-opacity', 1);
+		} else {
+			map.setPaintProperty('d4-fill', 'fill-opacity', 0);
+			map.setPaintProperty('d4-stroke', 'line-opacity', 0);
+		}
+
+		if (street.shown) {
+			map.setPaintProperty('onstreet', 'circle-opacity', 0.5);
+		} else {
+			map.setPaintProperty('onstreet', 'circle-opacity', 0);
+		}
+
+		if (bikeRoadSpeed.shown) {
+			map.setPaintProperty('bike-stroke', 'line-color', [
+				'match',
+				['get', 'speed'],
+				35,
+				'#8338ec',
+				30,
+				'#3a86ff',
+				/* other */ '#ff006e'
+			]);
+			map.setPaintProperty('bike-stroke', 'line-width', [
+				'match',
+				['get', 'speed'],
+				35,
+				3,
+				30,
+				2,
+				/* other */ 1
+			]);
+			map.setPaintProperty('bike-stroke', 'line-opacity', [
+				'match',
+				['get', 'withinD4'],
+				'true',
+				1,
+				/* other */ 0.2
+			]);
+		} else if (bike.shown && !bikeRoadSpeed.shown) {
+			map.setPaintProperty('bike-stroke', 'line-color', '#3a86ff')
+			map.setPaintProperty('bike-stroke', 'line-width', 2)
+			map.setPaintProperty('bike-stroke', 'line-opacity', [
+				'match',
+				['get', 'withinD4'],
+				'true',
+				1,
+				/* other */ 0.2
+			]);
+		} else {
+			map.setPaintProperty('bike-stroke', 'line-opacity', 0);
+		}
+	});
 
 	function handleMapLoad(e) {
-		map = (window.map = e.detail);
+		map = window.map = e.detail;
 		map.on('click', function handleClick(e) {
 			console.log(e.lngLat);
 		});
 
-		map.fitBounds([
-			[bbox[0], bbox[1]],
-			[bbox[2], bbox[3]],
-		], {
-			padding: 30
-		})
+		map.fitBounds(
+			[
+				[bbox[0], bbox[1]],
+				[bbox[2], bbox[3]]
+			],
+			{
+				padding: 30
+			}
+		);
 
 		map.addSource('d4', {
 			type: 'geojson',
 			data: d4
-		})
+		});
 
 		map.addSource('bike', {
 			type: 'geojson',
 			data: bike
-		})
+		});
 
 		map.addSource('onstreet', {
 			type: 'geojson',
-			data: onstreet,
-		})
+			data: onstreet
+		});
 
 		map.addSource('signals', {
 			type: 'geojson',
 			data: signals
-		})
+		});
 
 		map.addLayer({
 			id: 'd4-fill',
@@ -57,9 +133,9 @@
 			type: 'fill',
 			paint: {
 				'fill-color': '#000000',
-				'fill-opacity': .1
+				'fill-opacity': 0.1
 			}
-		})
+		});
 
 		map.addLayer({
 			id: 'd4-stroke',
@@ -69,7 +145,7 @@
 				'line-color': '#000000',
 				'line-width': 2
 			}
-		})
+		});
 
 		map.addLayer({
 			id: 'bike-stroke',
@@ -85,24 +161,10 @@
 					'#3a86ff',
 					/* other */ '#ff006e'
 				],
-				'line-width': [
-					'match',
-					['get', 'speed'],
-					35,
-					3,
-					30,
-					2,
-					/* other */ 1
-				],
-				'line-opacity': [
-					'match',
-					['get', 'withinD4'],
-					'true',
-					1,
-					/* other */ .2
-				]
+				'line-width': ['match', ['get', 'speed'], 35, 3, 30, 2, /* other */ 1],
+				'line-opacity': 0 //['match', ['get', 'withinD4'], 'true', 1, /* other */ 0.2]
 			}
-		})
+		});
 
 		map.addLayer({
 			id: 'onstreet',
@@ -113,30 +175,7 @@
 				'circle-color': 'purple',
 				'circle-opacity': 0
 			}
-		})
-
-		// map.addLayer({
-		// 	id: 'signals',
-		// 	source: 'signals',
-		// 	type: 'circle',
-		// 	paint: {
-		// 		'circle-radius': 3.3,
-		// 		'circle-color': [
-		// 			'match',
-		// 			['get', 'withinD4'],
-		// 			'true',
-		// 			'rebeccapurple',
-		// 			/* other */ '#000000'
-		// 		],
-		// 		'circle-opacity': [
-		// 			'match',
-		// 			['get', 'withinD4'],
-		// 			'true',
-		// 			1,
-		// 			/* other */ 0
-		// 		]
-		// 	}
-		// })
+		});
 	}
 </script>
 
@@ -145,17 +184,36 @@
 </svelte:head>
 
 <section>
-	<img src="https://www.cityofsacramento.org/-/media/Corporate/Files/Public-Works/Transportation/VisionZero/15mph-School-Zone-Citywide-Fact-Sheet-2.jpg?h=325&w=600&la=en">
+	<h1>District 4 bike infrastructure</h1>
+	<p><a href="https://www.cityofsacramento.org/-/media/Corporate/Files/Public-Works/Transportation/VisionZero/15mph-School-Zone-Citywide-Fact-Sheet-2.jpg?h=325&w=600&la=en">According to the City of Sacramento</a>, there is a 40% chance of a pedestrian or biker being killed if they are struck by a car going 30 MPH, which falls to a 5% chance of death if the car is going 20 MPH.</p>
 	<div class="layers">
-		<ul>
-			{#each layers as layer}
-			<li>
-				<button>
-					{layer.label}
-				</button>
-			</li>
-			{/each}
-		</ul>
+		<form
+			on:change={(e) => {
+				const { id, checked } = e.target;
+				const match = $layerToggles.find((d) => d.id === id);
+
+				match.shown = checked;
+
+				$layerToggles = $layerToggles;
+			}}
+		>
+			<fieldset>
+				<legend>Show layers</legend>
+
+				{#each $layerToggles as layer}
+					<div>
+						<input
+							type="checkbox"
+							value={layer.id}
+							name={layer.id}
+							id={layer.id}
+							checked={layer.shown}
+						/>
+						<label for={layer.id}>{layer.label}</label>
+					</div>
+				{/each}
+			</fieldset>
+		</form>
 	</div>
 	<div class="map-container">
 		<MapboxMap
@@ -172,10 +230,10 @@
 		--min-height: 700px;
 	}
 
-	.layers {
-		ul {
-			list-style-type: none;
-			padding: 0;
-		}
+	fieldset {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin-bottom: 1.2rem;
 	}
 </style>
